2ed1f9fcf336f1e686292beb97e615b897328567
Antonino Cucchiara <antonio_cuc@yahoo.co.uk>
2015-07-07 12:52:38 +0100

    Fix markup on XHtml Outlet

--- Core/Outlet/Xhtml/Page.php
+++ Core/Outlet/Xhtml/Page.php
@@ -1,22 +1,10 @@
 <html>
-	<style>
-		body
-		{
-			font-family: Verdana, Arial, Sans-Serif;
-			font-size: 95%;
-			width: 500px;
-			margin: 10px auto;
-		}
-	</style>
-	<title>
-		<?php echo $page_data['title']; ?>
-	</title>
+	<head>
+		<title>
+			<?php echo $page_data['title']; ?>
+		</title>
+	</head>
 	<body>
-		<h1>Site title</h1>
-		<p>Motto</p>
-		<hr />
 		<?php echo $page_data['content']; ?>
-		<hr />
-		<p>Footer</p>
 	</body>
 </html>
